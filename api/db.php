31268d20eed84dc8546f3ea2b94d5317501cc1de
DzakaAl <121382679+DzakaAl@users.noreply.github.com>
2025-06-19 19:00:49 +0700

    Update db.php

--- api/db.php
+++ api/db.php
@@ -2,20 +2,16 @@
 header('Access-Control-Allow-Origin: *'); // Ganti dengan alamat frontend Anda jika berbeda
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE'); // Metode yang diizinkan
 header('Access-Control-Allow-Headers: Content-Type');
-
 // db.php - Database connection script
-// Ambil URL koneksi database dari variabel lingkungan
-$databaseUrl = parse_url(getenv('mysql://root:VbdySdDnYXTCxzoaXBTIFvbwmAhXZHHo@caboose.proxy.rlwy.net:10772/railway'));
-
-// Koneksi ke MySQL menggunakan PDO
-$host = $databaseUrl['host'];
-$dbname = ltrim($databaseUrl['path'], '/'); // Mengambil nama database
-$username = $databaseUrl['user'];
-$password = $databaseUrl['pass'];
+$host = '127.0.0.1';      // Database host (use '127.0.0.1' if 'localhost' doesn't work)
+$port = '4040';
+$dbname = 'badminton';    // Your database name
+$username = 'root';       // Your database username
+$password = '';           // Your database password
 
 // Create a PDO instance to connect to the database
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
@@ -24,3 +20,4 @@ try {
     exit; // Stop execution if connection fails
 }
 ?>
+z
